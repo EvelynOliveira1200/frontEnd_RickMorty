6316007e9604bd7cd734871b3f91ae37e5369ee8
Evelyn <145203060+EvelynOliveira1200@users.noreply.github.com>
2025-09-02 23:35:18 -0300

    feat: update page infoApi

--- src/app/infoApi/page.jsx
+++ src/app/infoApi/page.jsx
@@ -4,18 +4,32 @@ import styles from './infoApi.module.css'
 import Image from 'next/image'
 
 export default function Home() {
+  const characters = [
+    { name: "Rick Sanchez", img: "/image/rick.png" },
+    { name: "Morty Smith", img: "/image/morty.png" },
+    { name: "Summer Smith", img: "/image/summer.png" },
+    { name: "Birdperson", img: "/image/birdperson.png" }
+  ]
+
   return (
     <div className={styles.container}>
-      <div className={styles.header}>
-        <Image src="/image/logo.png" alt="Logo" width={200} height={200} />
-        <li className={styles.menuItem}>Personagens</li>
-        <li className={styles.menuItem}>Detalhes</li>
-        <li className={styles.menuItem}>Sobre mim</li>
-      </div>
+      <header className={styles.header}>
+        <Image src="/image/logo.png" alt="Logo" width={220} height={220} className={styles.logo} />
+        <ul className={styles.menu}>
+          <li className={styles.menuItem}>Personagens</li>
+          <li className={styles.menuItem}>Detalhes</li>
+          <li className={styles.menuItem}>Sobre mim</li>
+        </ul>
+      </header>
 
-      <div className={styles.banner}>
-        <Image src="/image/background0.jpg" alt="Banner" width={1920} height={600} className={styles.bannerImg} />
-      </div>
+      <section className={styles.banner}>
+        <Image src="/image/banner01.png" alt="Banner" width={1920} height={1080} className={styles.bannerImg} />
+        <div className={styles.bannerContent}>
+          <h1 className={styles.bannerTitle}>Bem-Vindo</h1>
+          <p className={styles.bannerDescription}>A Rick and Morty API é gratuita e pública, oferecendo dados sobre personagens, episódios e locais da série. Ideal para estudos, consumo via REST e exploração do multiverso da animação de forma simples e acessível.</p>
+         <button className={styles.ctaButton}>Explorar Personagens</button></div>
+       
+      </section>
     </div>
   )
 }
