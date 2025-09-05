f460deff1107adf1630a98b23c97354a217bd77b
Evelyn <145203060+EvelynOliveira1200@users.noreply.github.com>
2025-09-04 21:58:08 -0300

    feat: updtade page infoApi

--- src/app/infoApi/page.jsx
+++ src/app/infoApi/page.jsx
@@ -1,20 +1,27 @@
-"use client"
-import React from 'react'
-import styles from './infoApi.module.css'
-import Image from 'next/image'
+
+"use client";
+import React from "react";
+import styles from "./infoApi.module.css";
+import Image from "next/image";
 
 export default function Home() {
   const characters = [
     { name: "Rick Sanchez", img: "/image/rick.png" },
     { name: "Morty Smith", img: "/image/morty.png" },
     { name: "Summer Smith", img: "/image/summer.png" },
-    { name: "Birdperson", img: "/image/birdperson.png" }
-  ]
+    { name: "Birdperson", img: "/image/birdperson.png" },
+  ];
 
   return (
     <div className={styles.container}>
       <header className={styles.header}>
-        <Image src="/image/logo.png" alt="Logo" width={220} height={220} className={styles.logo} />
+        <Image
+          src="/image/logo.png"
+          alt="Logo"
+          width={220}
+          height={220}
+          className={styles.logo}
+        />
         <ul className={styles.menu}>
           <li className={styles.menuItem}>Personagens</li>
           <li className={styles.menuItem}>Detalhes</li>
@@ -23,13 +30,77 @@ export default function Home() {
       </header>
 
       <section className={styles.banner}>
-        <Image src="/image/banner01.png" alt="Banner" width={1920} height={1080} className={styles.bannerImg} />
+        <Image
+          src="/image/banner01.png"
+          alt="Banner"
+          width={1920}
+          height={1080}
+          className={styles.bannerImg}
+        />
         <div className={styles.bannerContent}>
           <h1 className={styles.bannerTitle}>Bem-Vindo</h1>
-          <p className={styles.bannerDescription}>A Rick and Morty API é gratuita e pública, oferecendo dados sobre personagens, episódios e locais da série. Ideal para estudos, consumo via REST e exploração do multiverso da animação de forma simples e acessível.</p>
-         <button className={styles.ctaButton}>Explorar Personagens</button></div>
-       
+          <p className={styles.bannerDescription}>
+            A Rick and Morty API é gratuita e pública, oferecendo dados sobre
+            personagens, episódios e locais da série. Ideal para estudos,
+            consumo via REST e exploração do multiverso da animação de forma
+            simples e acessível.
+          </p>
+          <button className={styles.ctaButton}>Explorar Personagens</button>
+        </div>
+      </section>
+
+      <section className={styles.infoApi}>
+        <div className={styles.divInfoApi}>
+          <h2 className={styles.infoApiTitle}>Sobre Api</h2>
+          <p className={styles.infoApiDescription}>
+            A Rick and Morty API é uma API RESTful gratuita e pública que
+            fornece informações detalhadas sobre personagens, episódios e
+            locais da série de animação "Rick and Morty". Ela é amplamente
+            utilizada por desenvolvedores para criar aplicativos, sites e
+            projetos relacionados à série. A API é fácil de usar e oferece
+            dados estruturados em formato JSON, tornando-a acessível para uma
+            variedade de linguagens de programação e plataformas.
+          </p>
+          <ul className={styles.infoApiList}>
+            <h3 className={styles.infoApiListTitle}>📱 Lista de Atributos da API</h3>
+            <li className={styles.infoApiListItem}>Personagens</li>
+            <li className={styles.infoApiListItem}>Episódios</li>
+            <li className={styles.infoApiListItem}>Localização</li>
+          </ul>
+          <h3 className={styles.infoApiListTitle}>🔗 URL base usada para o axios</h3>
+          <div className={styles.infoApiExample}>
+            <p className={styles.infoApiUrl}>
+              https://api.sampleapis.com/rickandmorty/
+            </p>
+          </div>
+
+          <h3 className={styles.infoApiListTitle}>📍 Exemplo de endpoint</h3>
+          <div className={styles.infoApiExample}>
+            <p className={styles.infoApiUrl}>
+              https://api.sampleapis.com/rickandmorty/characters
+            </p>
+          </div>
+
+          <div className={styles.infoApiExample}>
+            <p>https://api.sampleapis.com/rickandmorty/episodes</p>
+          </div>
+
+          <div className={styles.infoApiExample}>
+            <p className={styles.infoApiUrl}>
+              https://api.sampleapis.com/rickandmorty/locations
+            </p>
+          </div>
+        </div>
+        <div className={styles.imgInfo}>
+          <Image
+            src="/image/img.jpg"
+            alt="Info Api"
+            width={600}
+            height={400}
+            className={styles.infoApiImg}
+          />
+        </div>
       </section>
     </div>
-  )
+  );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className='min-h-screen bg-gradient-to-r from-blue-100 to-blue-200 flex items-center justify-center p-6'>
+    <div className='min-h-screen bg-gradient-to-r from-black to-gray-900 flex items-center justify-center p-6'>
       <div className='max-w-4xl bg-white p-8 rounded-xl shadow-2xl flex flex-col sm:flex-row items-center'>
         <Image 
           src="/image/perfil_img.jpeg" 
